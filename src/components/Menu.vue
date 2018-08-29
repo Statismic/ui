bc62d1777f15cf00060383b8a1aa52179c794ade
Lukas Herman <lherman.cs@gmail.com>
2018-08-29 12:47:51 -0400

    Initial commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="card">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  }
+};
+</script>
+
+<style scoped>
+#card {
+  cursor: pointer;
+  padding: 15px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 5px;
+
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
+
+#card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Menu v-for="(value, index) in menus" :key="index" :name="value" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Menu from "@/components/Menu.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Menu
+  },
+  data() {
+    let menus = [];
+    for (let i = 1; i <= 30; i++) menus.push(i.toString());
+    return {
+      menus: menus
+    };
   }
 };
 </script>
+
+<style scoped>
+.container {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fit, minmax(155px, 1fr));
+  justify-items: stretch;
+}
+</style>
