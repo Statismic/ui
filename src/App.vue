5a83d23f81e7f1185efedfc4b53e8143583a825d
Lukas Herman <lherman.cs@gmail.com>
2018-09-07 20:04:07 -0400

    Added experimental update notification

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
+<div>
+  <Notification/>
   <router-view/>
+</div>
 </template>
 
 <script>
+import Notification from "@/components/Notification.vue";
+
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    Notification
+  }
 };
 </script>
 
@@ -17,5 +25,6 @@ export default {
 
 body {
   background-color: #005960;
+  margin: 0px;
 }
 </style>
