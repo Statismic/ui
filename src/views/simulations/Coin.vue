d074aa37553a6f0af61142da508cb581f82ec79f
Gabrielle Stewart <gestewa@clemson.edu>
2018-09-16 22:20:51 -0400

    Converted Coin Page to CSS grid

--- src/views/simulations/Coin.vue
+++ src/views/simulations/Coin.vue
@@ -1,24 +1,34 @@
 <template>
 <div>
-  <h1>
+
+  <header class="nav">
+    <h1 class="nav-logo">Statistismic
+      <img class="nav-logo" src="https://raw.githubusercontent.com/Statismic/ui/master/src/assets/logo/logo_basic.png" height=40px width=40px/>
+    </h1>
+  </header>
+
+  <h1 class="description">
     Coin Flip Simmulation
   </h1>
+  <div class="input">
+    <form action="#">
+        # of coins : <input type="number" v-model="coins"><br>
+        # of experiments : <input type="number" v-model="exper"><br>
+        probability of success (heads) : <input type="number" v-model="probability"><br>
+        hide coin visualization <input type="checkbox" v-model="hidden"><br>
+        <button type="button" @click="run">Run Experiment</button>
+    </form>
+      <!-- TODO restrict some input to decimal point -->
+  </div>
+  <div class="short-graph">This is the short-term Graph</div>
+  <div class="output">
+    <p v-show="!hidden">{{coinString}}</p>
+    <p v-html="timerString"></p>
+  </div>
+  <div class="long-graph">This is the long-term Graph</div>
+  <footer class="foot">Just pretend I'm a super rad footer</footer>
+  
 
-  <form action="#">
-    # of coins : <input type="number" v-model="coins"><br>
-    # of experiments : <input type="number" v-model="exper"><br>
-    probability of success (heads) : <input type="number" v-model="probability"><br>
-    hide coin visualization <input type="checkbox" v-model="hidden"><br>
-    <button type="button" @click="run">Run Experiment</button>
-
-</form>
-  <!-- TODO restrict some input to decimal point -->
-
-  <br>
-  <br>
-  <br>
-  <p v-show="!hidden">{{coinString}}</p>
-  <p v-html="timerString"></p>
 
 </div>
 </template>
@@ -34,43 +44,47 @@ export default {
       heads: 0,
       total: 0,
       exper: 1,
-      probability: .5,
+      probability: 0.5,
       hidden: false,
-      accuracy: 1000, //decimal accuracy s.t. we weight the random probability
+      accuracy: 1000 //decimal accuracy s.t. we weight the random probability
     };
   },
   methods: {
-    doExper(){
-      this.coinString = ""
-      let processedProb = Math.round(this.probability*this.accuracy)
-      if(this.coins<0||this.exper<0)
-        return
-      for(let j = 0; j<this.exper; j++){
-        for(let i = 0; i<this.coins; i++){
-          let x = (Math.round(Math.random()*this.accuracy)<= processedProb)
-          this.coinString+= +x
-          this.coinString+="\t"
-          this.total++
-          if(x===true)
-            this.heads++     
+    doExper() {
+      this.coinString = "";
+      let processedProb = Math.round(this.probability * this.accuracy);
+      if (this.coins < 0 || this.exper < 0) return;
+      for (let j = 0; j < this.exper; j++) {
+        for (let i = 0; i < this.coins; i++) {
+          let x = Math.round(Math.random() * this.accuracy) <= processedProb;
+          this.coinString += +x;
+          this.coinString += "\t";
+          this.total++;
+          if (x === true) this.heads++;
         }
       }
     },
-    run(){
+    run() {
       let t0 = performance.now();
       this.doExper();
       let t1 = performance.now();
-      this.timerString = "Call took " + (t1 - t0) + " milliseconds."+
-          "<br>Heads : "+ this.heads +
-          "<br>Total : "+ this.total +
-          "<br>Ratio : "+ (this.heads/this.total).toPrecision(5) 
+      this.timerString =
+        "Call took " +
+        (t1 - t0) +
+        " milliseconds." +
+        "<br>Heads : " +
+        this.heads +
+        "<br>Total : " +
+        this.total +
+        "<br>Ratio : " +
+        (this.heads / this.total).toPrecision(5);
     }
   },
   watch: {
     coins(val) {
       this.coins = Math.round(val);
     },
-    exper (val) {
+    exper(val) {
       this.exper = Math.round(val);
     }
   }
@@ -78,6 +92,50 @@ export default {
 </script>
 
 <style scoped>
-  div {background-color: lightblue;}
-</style>
+div {
+  line-height: 1.6;
+  background: lightblue;
 
+  display: grid;
+  grid-template:
+    "nav nav nav nav" auto
+    ". input short-graph ." 39vh
+    ". output long-graph ." 39vh
+    "foot foot foot foot" auto
+    / 1fr minmax(15em, 3fr) minmax(30em, 6fr) 1fr;
+  grid-column-gap: 1.5em;
+  grid-row-gap: 1.5em;
+}
+
+.nav {
+  grid-area: nav;
+  background: blue;
+}
+.nav-logo {
+  text-align: left;
+  color: white;
+}
+.description {
+  grid-area: description;
+}
+.input {
+  grid-area: input;
+  background: red;
+}
+.short-graph {
+  grid-area: short-graph;
+  background: green;
+}
+.output {
+  grid-area: output;
+  background: orange;
+}
+.long-graph {
+  grid-area: long-graph;
+  background: yellow;
+}
+.foot {
+  grid-area: foot;
+  background: blue;
+}
+</style>
