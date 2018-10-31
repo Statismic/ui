d179ea90155d140f47804bece5ab1865dbdbce12
Gabrielle Stewart <gestewa@clemson.edu>
2018-10-31 16:08:48 -0400

    Installed font awesome

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import plugins from "./core/plugins";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import "./registerServiceWorker";
 
+library.add(faSearch);
+
+Vue.component("fa", FontAwesomeIcon);
 Vue.use(VueParticles);
 Vue.use(plugins);
 Vue.config.productionTip = false;
