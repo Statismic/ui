6e1c8c22bf6f431110dfb9bf192f88a3f8b430a8
Lukas <lherman.cs@gmail.com>
2019-04-10 14:27:25 -0400

    Replaced SearchResult with vuetify

--- components/home/SearchResult.vue
+++ components/home/SearchResult.vue
@@ -1,23 +1,19 @@
 <template>
-  <div class="card">
-    <div class="container">
-      <h3 class="title">
-        Search Results
-      </h3>
-      <ul>
-        <li
-          v-for="(result, index) in results"
-          :key="index"
-        >
-          <nuxt-link :to="result.path">
-            <h4 class="result" style="text-transform: capitalize">
-              {{ result.name }}
-            </h4>
-          </nuxt-link>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <v-card style="width: fit-content;" class="pa-2">
+    <v-card-title primary-title>
+      <h1>Search Results</h1>
+    </v-card-title>
+
+    <v-divider />
+
+    <v-list>
+      <v-list-tile v-for="(result, index) in results" :key="index" router :to="result.path">
+        <v-list-tile-content>
+          <v-list-tile-title v-text="result.name" />
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
@@ -30,39 +26,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.card {
-  background-color: rgba(0, 0, 0, 0.5);
-  color: white;
-  cursor: pointer;
-  width: fit-content;
-  padding: 15px;
-  padding-left: 20px;
-  padding-right: 20px;
-  border-radius: 5px;
-  z-index: 10;
-
-  /* Add shadows to create the "card" effect */
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-}
-
-.card:hover {
-  box-shadow: 0 2px 4px 2px rgba(0, 0, 0, 0.2);
-}
-
-.container {
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 10px;
-  grid-auto-flow: dense;
-}
-
-.title {
-  color: white;
-}
-
-.result {
-  color: rgba(255, 255, 255, 0.8);
-}
-</style>
